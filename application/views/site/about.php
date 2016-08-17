9927160747a1a3336c70ad5608a5595db7d0a4a8
shihao <pk111100@gmail.com>
2016-08-17 15:29:03 +0800

    切版

--- application/views/site/about.php
+++ application/views/site/about.php
@@ -6,10 +6,9 @@
     <!-- Page-->
     <div class="page text-center">
       <!-- Page Header-->
-
       <div class="shell">
         <ul class="list-inline list-inline-dashed offset-top-25 breadcrumbs">
-          <li><a href="index.html">首頁</a></li>
+          <li><a href="<?=site_url('site/index')?>">首頁</a></li>
           <li class="active text-gray-dark">關於我們</li>
         </ul>
       </div>
@@ -26,10 +25,8 @@
             <div class="cell-sm-12 cell-md-12 offset-top-45 offset-md-top-0">
               <div class="offset-top-60 offset-md-top-45">
                 <div class="unit unit-xl-horizontal unit-lg-horizontal unit-md-horizontal unit-sm-vertical unit-xs-vertical unit-spacing-md">
-                  <div class="unit-left"><img src="<?=base_url()?>public/site/images/aboutus-01-550x550-1.jpg" width="470" height="470" alt="" class="img-responsive"/>
-                  </div>
                   <div class="unit-body">
-                    <!-- <h4 class="text-light">Dr. Mark Hoffman</h4> -->
+                    
                     <?=$about['content']?>
                   </div>
                 </div>
@@ -43,51 +40,12 @@
           </div>
         </section>
       </main>
-      <div class="footer-offset-none">
-      </div>
-    </div>
-    <!-- Global Mailform Output-->
-    <div id="form-output-global" class="snackbars"></div>
-    <!-- PhotoSwipe Gallery-->
-    <div tabindex="-1" role="dialog" aria-hidden="true" class="pswp">
-      <div class="pswp__bg"></div>
-      <div class="pswp__scroll-wrap">
-        <div class="pswp__container">
-          <div class="pswp__item"></div>
-          <div class="pswp__item"></div>
-          <div class="pswp__item"></div>
-        </div>
-        <div class="pswp__ui pswp__ui--hidden">
-          <div class="pswp__top-bar">
-            <div class="pswp__counter"></div>
-            <button title="Close (Esc)" class="pswp__button pswp__button--close"></button>
-            <button title="Share" class="pswp__button pswp__button--share"></button>
-            <button title="Toggle fullscreen" class="pswp__button pswp__button--fs"></button>
-            <button title="Zoom in/out" class="pswp__button pswp__button--zoom"></button>
-            <div class="pswp__preloader">
-              <div class="pswp__preloader__icn">
-                <div class="pswp__preloader__cut">
-                  <div class="pswp__preloader__donut"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-            <div class="pswp__share-tooltip"></div>
-          </div>
-          <button title="Previous (arrow left)" class="pswp__button pswp__button--arrow--left"></button>
-          <button title="Next (arrow right)" class="pswp__button pswp__button--arrow--right"></button>
-          <div class="pswp__caption">
-            <div class="pswp__caption__cent"></div>
-          </div>
-        </div>
-      </div>
     </div>
     <!-- Java script-->
     <?php 
     include("layout/footer.php");
     ?>
-    <script src="js/core.min.js"></script>
-    <script src="js/script.js"></script>
+<script src="<?=base_url()?>public/site/js/core.min.js"></script>
+<script src="<?=base_url()?>public/site/js/script.js"></script>
   </body>
 </html>
