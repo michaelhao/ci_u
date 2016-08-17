3a2e00329b9d6bd66aba518a90d5aae0553d33f8
shihao <pk111100@gmail.com>
2016-08-17 15:28:29 +0800

    資料修改

--- application/views/site/layout/meta.php
+++ application/views/site/layout/meta.php
@@ -1,8 +1,11 @@
+<?php
+$row=$this->db->get_where('backadmin', array('id'=>1))->row_array();
+?>
 <!DOCTYPE html>
 <html lang="en" class="wide wow-animation">
   <head>
     <!-- Site Title-->
-    <title>關於我們</title>
+    <title><?=$row['webtitle'];?></title>
     <meta name="format-detection" content="telephone=no">
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
