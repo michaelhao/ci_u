cc988847342ab7068b1855b5d86d64a23388b73e
shihao <pk111100@gmail.com>
2016-08-17 15:27:42 +0800

    資料更改

--- application/views/site/layout/footer.php
+++ application/views/site/layout/footer.php
@@ -1,19 +1,65 @@
-<footer class="page-footer text-center">
-          <div class="footer-container footer-copyright">
-            <div class="shell">
-              <div class="reveal-sm-flex range-sm-justify range-sm-reverse range-sm-middle">
-                <div>
-                          <ul class="list-inline list-inline-md">
-                            <li><a href="#" class="icon fa-facebook icon-default"></a></li>
-                            <li><a href="#" class="icon fa-twitter icon-default"></a></li>
-                            <li><a href="#" class="icon fa-google-plus icon-default"></a></li>
-                          </ul>
-                </div>
-                <div class="offset-top-15 offset-sm-top-0">
-                  <p>© 台灣暢宇國際有限公司 2016 ALL RIGHTS RESERVED
-                  </p>
-                </div>
-              </div>
+<?php
+$foots=$this->db->order_by('id','des')->get_where('backadmin', array(
+       'id' => 1, 
+   ))->result_array();
+?>
+
+<!-- Global Mailform Output-->
+<div id="form-output-global" class="snackbars"></div>
+<!-- PhotoSwipe Gallery-->
+<div tabindex="-1" role="dialog" aria-hidden="true" class="pswp">
+   <div class="pswp__bg"></div>
+   <div class="pswp__scroll-wrap">
+      <div class="pswp__container">
+         <div class="pswp__item"></div>
+         <div class="pswp__item"></div>
+         <div class="pswp__item"></div>
+      </div>
+      <div class="pswp__ui pswp__ui--hidden">
+         <div class="pswp__top-bar">
+            <div class="pswp__counter"></div>
+            <button title="Close (Esc)" class="pswp__button pswp__button--close"></button>
+            <button title="Share" class="pswp__button pswp__button--share"></button>
+            <button title="Toggle fullscreen" class="pswp__button pswp__button--fs"></button>
+            <button title="Zoom in/out" class="pswp__button pswp__button--zoom"></button>
+            <div class="pswp__preloader">
+               <div class="pswp__preloader__icn">
+                  <div class="pswp__preloader__cut">
+                     <div class="pswp__preloader__donut"></div>
+                  </div>
+               </div>
             </div>
-          </div>
-        </footer>
+         </div>
+         <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+            <div class="pswp__share-tooltip"></div>
+         </div>
+         <button title="Previous (arrow left)" class="pswp__button pswp__button--arrow--left"></button>
+         <button title="Next (arrow right)" class="pswp__button pswp__button--arrow--right"></button>
+         <div class="pswp__caption">
+            <div class="pswp__caption__cent"></div>
+         </div>
+      </div>
+   </div>
+</div>
+
+<footer class="page-footer text-center">
+   <div class="footer-container footer-copyright">
+      <div class="shell">
+         <div class="reveal-sm-flex range-sm-justify range-sm-reverse range-sm-middle">
+            <div>
+               <ul class="list-inline list-inline-md">
+                  <li><a href="#" class="icon fa-facebook icon-default"></a></li>
+                  <li><a href="#" class="icon fa-twitter icon-default"></a></li>
+                  <li><a href="#" class="icon fa-google-plus icon-default"></a></li>
+               </ul>
+            </div>
+            <div class="offset-top-15 offset-sm-top-0">
+            <?php foreach ($foots as $key => $foot): ?>
+               <p><?=$foot['copyright']?></p> 
+            <?php endforeach ?>
+               
+            </div>
+         </div>
+      </div>
+   </div>
+</footer>
