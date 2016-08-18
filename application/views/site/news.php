a86a4b6f7a4906759b2a02403b831a4ef5966d7c
shihao <pk111100@gmail.com>
2016-08-18 16:25:32 +0800

    內容修正 & 套用分頁style

--- application/views/site/news.php
+++ application/views/site/news.php
@@ -1,118 +1,83 @@
 <?php
-    include("layout/meta.php");
-    include("layout/header.php");
-?>
+   include("layout/meta.php");
+   include("layout/header.php");
+   ?>
+   
+<style type="text/css">
+   .pagination > .next_style > a{
+    color :#0095e5;
+    font-style: normal;
+    font-weight: 800;}
+  .pagination > .last_style > a{
+    color :#0095e5;
+    font-style: normal;
+    font-weight: 800;}
+  .pagination .next_style:after {
+    content: '\f105';
+    font-family: 'FontAwesome';
+    color: #616262;
+    padding-left: 7px;
+    position: relative;
+    top: 1px;}
+  .pagination .last_style:before {
+    content: '\f104';
+    font-family: 'FontAwesome';
+    color: #616262;
+    padding-right: 7px;
+    position: relative;
+    top: 1px;}
+</style>
 <!-- Page-->
-    <div class="page text-center">
-      <!-- Page Header-->
-      <div class="shell">
-        <ul class="list-inline list-inline-dashed offset-top-25 breadcrumbs">
-          <li><a href="index.html">首頁</a></li>
-          <li class="active text-gray-dark">最新消息</li>
-        </ul>
-      </div>
-      <!-- Page Content-->
-      <main class="page-content">
-        <div id="fb-root"></div>
-        <div class="shell offset-top-25 offset-md-top-45">
-          <h3 class="text-center">最新消息</h3>
-          <div class="range range-xs-center offset-top-25 offset-md-top-35">
+<div class="page text-center">
+   <!-- Page Header-->
+   <div class="shell">
+      <ul class="list-inline list-inline-dashed offset-top-25 breadcrumbs">
+         <li><a href="<?=site_url('site/index')?>">首頁</a></li>
+         <li class="active text-gray-dark">最新消息</li>
+      </ul>
+   </div>
+   <!-- Page Content-->
+   <main class="page-content">
+      <div id="fb-root"></div>
+      <div class="shell offset-top-25 offset-md-top-45">
+         <h3 class="text-center">最新消息</h3>
+         <div class="range range-xs-center offset-top-25 offset-md-top-35">
             <!-- Posts-->
             <div class="cell-sm-12 cell-md-12 offset-top-5">
-              <div class="post post-list">
-              
-                <h4 class="text-bold"><a href="" class="post-title">8月8日父親節。熊衛士親子戶外運動日</a></h4>
-                <ul class="list-inline list-inline-dashed text-italic">
-
-                  <li>
-                    <time datetime="2016-04-30" class="post-meta-time text-gray-lighter">May 10, 2016</time>
-                  </li>
-                </ul>
-                <div class="post-media-wrap"><a href=""><img src="<?=base_url()?>public/site/images/news-01.jpg"  alt="" class="img-responsive post-image"/></a>
-                </div>
-                <p>我們將在8/8在大佳河濱公園舉辦熊衛士親子戶外活動日，歡迎踴躍參加。lacerat lorem turpis at. Aliquet lorem porttitor interdum. Amet lacus. Aliquam lobortis faucibus blandit ac phasellus. In amet magna non interdum volutpat porttitor metus a ante ac neque. Nisi turpis. Commodo col. Interdum adipiscing mollis ut aliquam id ante adipiscing commodo integer arcu amet Ac interdum ac non praesent. Cubilia lacinia interdum massa faucibus blandit nullam. Accumsan phasellus nunc integer. Accumsan euismod nunc adipiscing lacinia erat ut sit. Arcu amet. Id massa aliquet arcu accumsan lorem amet accumsan commodo odio cubilia ac eu interdum placerat placerat arcu commodo lobortis adipiscing semper ornare pellentesque.
-Amet nibh adipiscing adipiscing. Commodo ante vis placerat interdum massa massa primis. Tempus condimentum tempus non ac varius cubilia adipiscing placerat lorem turpis at. Aliquet lorem porttitor interdum. Amet lacus. Aliquam lobortis faucibus blandit ac phasellus. In amet magna non interdum volutpat porttitor metus a ante ac neque. Nisi turpis. Commodo col. Interdum adipiscing mollis ut aliquam id ante adipiscing commodo integer arcu amet blandit adipiscing arcu ante.</p>
-                <BR>
-                <p>新顏色帶來新心情！寶貝們可以自行挑選喜愛的顏色搭配！lacerat lorem turpis at. Aliquet lorem porttitor interdum. Amet lacus. Aliquam lobortis faucibus blandit ac phasellus. In amet magna non interdum volutpat porttitor metus a ante ac neque. Nisi turpis. Commodo col. Interdum adipiscing mollis ut aliquam id ante adipiscing commodo integer arcu amet Ac interdum ac non praesent. Cubilia lacinia interdum massa faucibus blandit nullam. Accumsan phasellus nunc integer. Accumsan euismod nunc adipiscing lacinia erat ut sit. Arcu amet. Id massa aliquet arcu accumsan lorem amet accumsan commodo odio cubilia ac eu interdum placerat placerat arcu commodo lobortis adipiscing semper ornare pellentesque.</p>
-              </div>
-              <hr class="divider"/>
-            <div class="cell-sm-12 cell-md-12 offset-top-5">
-              <div class="post post-list">
-                <h4 class="text-bold"><a href="" class="post-title">8月8日父親節。熊衛士親子戶外運動日</a></h4>
-                <ul class="list-inline list-inline-dashed text-italic">
-
-                  <li>
-                    <time datetime="2016-04-30" class="post-meta-time text-gray-lighter">May 10, 2016</time>
-                  </li>
-                </ul>
-                <div class="post-media-wrap"><a href=""><img src="<?=base_url()?>public/site/images/news-02.jpg"  alt="" class="img-responsive post-image"/></a>
-                </div>
-                <p>我們將在8/8在大佳河濱公園舉辦熊衛士親子戶外活動日，歡迎踴躍參加。lacerat lorem turpis at. Aliquet lorem porttitor interdum. Amet lacus. Aliquam lobortis faucibus blandit ac phasellus. In amet magna non interdum volutpat porttitor metus a ante ac neque. Nisi turpis. Commodo col. Interdum adipiscing mollis ut aliquam id ante adipiscing commodo integer arcu amet Ac interdum ac non praesent. Cubilia lacinia interdum massa faucibus blandit nullam. Accumsan phasellus nunc integer. Accumsan euismod nunc adipiscing lacinia erat ut sit. Arcu amet. Id massa aliquet arcu accumsan lorem amet accumsan commodo odio cubilia ac eu interdum placerat placerat arcu commodo lobortis adipiscing semper ornare pellentesque.
-Amet nibh adipiscing adipiscing. Commodo ante vis placerat interdum massa massa primis. Tempus condimentum tempus non ac varius cubilia adipiscing placerat lorem turpis at. Aliquet lorem porttitor interdum. Amet lacus. Aliquam lobortis faucibus blandit ac phasellus. In amet magna non interdum volutpat porttitor metus a ante ac neque. Nisi turpis. Commodo col. Interdum adipiscing mollis ut aliquam id ante adipiscing commodo integer arcu amet blandit adipiscing arcu ante.</p>
-                <BR>
-                <p>新顏色帶來新心情！寶貝們可以自行挑選喜愛的顏色搭配！lacerat lorem turpis at. Aliquet lorem porttitor interdum. Amet lacus. Aliquam lobortis faucibus blandit ac phasellus. In amet magna non interdum volutpat porttitor metus a ante ac neque. Nisi turpis. Commodo col. Interdum adipiscing mollis ut aliquam id ante adipiscing commodo integer arcu amet Ac interdum ac non praesent. Cubilia lacinia interdum massa faucibus blandit nullam. Accumsan phasellus nunc integer. Accumsan euismod nunc adipiscing lacinia erat ut sit. Arcu amet. Id massa aliquet arcu accumsan lorem amet accumsan commodo odio cubilia ac eu interdum placerat placerat arcu commodo lobortis adipiscing semper ornare pellentesque.</p>
-              </div>
-              <div class="div offset-top-30 offset-md-top-40">
-                <!-- Pagination-->
-                <ul class="pagination">
-                  <li class="active"><span>1</span></li>
-                  <li><a href="#">2</a></li>
-                  <li><a href="#">3</a></li>
-                  <li><a href="#">4</a></li>
-                  <li><a href="#">5</a></li>
-                  <li class="veil reveal-sm-inline-block"><span>...</span></li>
-                  <li class="veil reveal-sm-inline-block"><a href="#">125</a></li>
-                  <li><a href="#" class="text-primary next-link text-extra-bold">Next</a></li>
-                </ul>
-                <BR><BR>
-              </div>
+               <?php foreach ($news as $key => $new): ?>
+               <article class="blog-item">
+                  <hr class="divider"/>
+                  <div class="post post-list">
+                     <div class="info-post">
+                        <h4 class="text-bold"><a href="" class="post-title"><?=$new['name']?></a></h4>
+                        <div class="post-meta-time text-gray-lighter">
+                           <p><?=date('F d,Y', strtotime($new['start_at']))?></p>
+                        </div>
+                        <div class="post-media-wrap"><img src="<?=$new['pic']?>" class="img-responsive post-image"/></a>
+                        </div>
+                        <div class="content-post">
+                           <p><?=$new['content']?></p>
+                        </div>
+                     </div>
+                  </div>
+               </article>
+               <?php endforeach ?>
+               <BR>
+               <nav class="pagination">
+                  <?php echo $this->pagination->create_links();?>
+               </nav>
+                </BR>
+               <BR><BR>
             </div>
-          </div>
-        </div>
-      </main>
-    </div>
-    <!-- Global Mailform Output-->
-    <div id="form-output-global" class="snackbars"></div>
-    <!-- PhotoSwipe Gallery-->
-    <div tabindex="-1" role="dialog" aria-hidden="true" class="pswp">
-      <div class="pswp__bg"></div>
-      <div class="pswp__scroll-wrap">
-        <div class="pswp__container">
-          <div class="pswp__item"></div>
-          <div class="pswp__item"></div>
-          <div class="pswp__item"></div>
-        </div>
-        <div class="pswp__ui pswp__ui--hidden">
-          <div class="pswp__top-bar">
-            <div class="pswp__counter"></div>
-            <button title="Close (Esc)" class="pswp__button pswp__button--close"></button>
-            <button title="Share" class="pswp__button pswp__button--share"></button>
-            <button title="Toggle fullscreen" class="pswp__button pswp__button--fs"></button>
-            <button title="Zoom in/out" class="pswp__button pswp__button--zoom"></button>
-            <div class="pswp__preloader">
-              <div class="pswp__preloader__icn">
-                <div class="pswp__preloader__cut">
-                  <div class="pswp__preloader__donut"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-            <div class="pswp__share-tooltip"></div>
-          </div>
-          <button title="Previous (arrow left)" class="pswp__button pswp__button--arrow--left"></button>
-          <button title="Next (arrow right)" class="pswp__button pswp__button--arrow--right"></button>
-          <div class="pswp__caption">
-            <div class="pswp__caption__cent"></div>
-          </div>
-        </div>
+         </div>
       </div>
-    </div>
-    <!-- Java script-->
-    <?php 
-    include("layout/footer.php");
-    ?>
-    <script src="js/core.min.js"></script>
-    <script src="js/script.js"></script>
-  </body>
+   </main>
+</div>
+<!-- Java script-->
+<?php 
+   include("layout/footer.php");
+   ?>
+<script src="<?=base_url()?>public/site/js/core.min.js"></script>
+<script src="<?=base_url()?>public/site/js/script.js"></script>
+</body>
 </html>
