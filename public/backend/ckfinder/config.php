5faf002b8f53eb83ca6b94602be3fec34f4c3174
shihao <pk111100@gmail.com>
2016-08-18 16:19:26 +0800

    更改圖片大小上限

--- public/backend/ckfinder/config.php
+++ public/backend/ckfinder/config.php
@@ -104,8 +104,8 @@ Set the maximum size of uploaded images. If an uploaded image is larger, it
 gets scaled down proportionally. Set to 0 to disable this feature.
 */
 $config['Images'] = Array(
-		'maxWidth' => 1600,
-		'maxHeight' => 1920,
+		'maxWidth' => 1920,
+		'maxHeight' => 16000,
 		'quality' => 80);
 
 /*
